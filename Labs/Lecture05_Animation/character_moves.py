d0d155ceb2606d597bc45585b7d9f78d4f621b0f
Daehyunlee@Alienware <daehyun.lee@gmail.com>
2023-09-20 08:22:31 +0900

    LEC 05 Animation Labs 추가

---
+++ Labs/Lecture05_Animation/character_moves.py
@@ -0,0 +1,11 @@
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+# 여기를 채우세요.
+
+close_canvas()
+
---
+++ Labs/Lecture05_Animation/character_runs.py
@@ -0,0 +1,12 @@
+from pico2d import *
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('run_animation.png')
+
+x = 0
+
+# 여기를 채우세요.
+
+
+close_canvas()
+
