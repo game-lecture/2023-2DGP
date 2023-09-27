f64263e6d5b5ca9e3c93ae01c15b7a4d8e8f3b9f
Daehyunlee@Alienware <daehyun.lee@gmail.com>
2023-09-27 12:36:43 +0900

    LEC 07 직선 이동 UPLOAD

---
+++ Labs/Lecture07_Linear_Movement/line.py
@@ -0,0 +1,64 @@
+import turtle
+import random
+
+
+def stop():
+    turtle.bye()
+
+
+def prepare_turtle_canvas():
+    turtle.setup(1024, 768)
+    turtle.bgcolor(0.2, 0.2, 0.2)
+    turtle.penup()
+    turtle.hideturtle()
+    turtle.shape('arrow')
+    turtle.shapesize(2)
+    turtle.pensize(5)
+    turtle.color(1, 0, 0)
+    turtle.speed(100)
+    turtle.goto(-500, 0)
+    turtle.pendown()
+    turtle.goto(480, 0)
+    turtle.stamp()
+    turtle.penup()
+    turtle.goto(0, -360)
+    turtle.pendown()
+    turtle.goto(0, 360)
+    turtle.setheading(90)
+    turtle.stamp()
+    turtle.penup()
+    turtle.home()
+
+    turtle.shape('circle')
+    turtle.pensize(1)
+    turtle.color(0, 0, 0)
+    turtle.speed(50)
+
+    turtle.onkey(stop, 'Escape')
+    turtle.listen()
+
+
+def draw_big_point(p):
+    turtle.goto(p)
+    turtle.color(0.8, 0.9, 0)
+    turtle.dot(15)
+    turtle.write('     '+str(p))
+
+
+def draw_point(p):
+    turtle.goto(p)
+    turtle.dot(5, random.random(), random.random(), random.random())
+
+
+def draw_line(p1, p2):
+    # fill here
+    pass
+
+
+prepare_turtle_canvas()
+
+
+# fill here
+
+
+turtle.done()
---
+++ Labs/Lecture07_Linear_Movement/move_character_with_mouse.py
@@ -0,0 +1,43 @@
+from pico2d import *
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+
+TUK_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+
+
+def handle_events():
+    global running
+    global x, y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+    pass
+
+
+
+running = True
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+frame = 0
+hide_cursor()
+
+while running:
+    clear_canvas()
+    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+
+    handle_events()
+
+close_canvas()
+
+
+
+
