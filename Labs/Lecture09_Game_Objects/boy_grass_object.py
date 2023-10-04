f3231a77a0091b94dbfabd24bafda4c8ac694be7
dustinlee@alienware <daehyun.lee.dev@gmail.com>
2023-10-04 09:50:49 +0900

    LEC 09 추가

---
+++ Labs/Lecture09_Game_Objects/boy_grass_object.py
@@ -0,0 +1,22 @@
+from pico2d import *
+
+# Game object class here
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+open_canvas()
+
+# initialization code
+
+# game main loop code
+
+# finalization code
+
+close_canvas()
