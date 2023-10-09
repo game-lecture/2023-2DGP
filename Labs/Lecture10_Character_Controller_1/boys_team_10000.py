df5c0fe1b7261266b2c3585b8705280277a98d60
dustinlee@alienware <daehyun.lee.dev@gmail.com>
2023-10-09 09:25:51 +0900

    LEC 10 캐릭터컨트롤러 (1) 추가

---
+++ Labs/Lecture10_Character_Controller_1/boys_team_10000.py
@@ -0,0 +1,80 @@
+from pico2d import *
+import random
+
+
+# Game object class here
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    def update(self):
+        pass
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 800), 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
+def reset_world():
+    global running
+    global grass
+    global team
+    global world
+
+    running = True
+    world = []
+
+    grass = Grass()
+    world.append(grass)
+
+    team = [Boy() for i in range(10)]
+    world += team
+
+
+def update_world():
+    for o in world:
+        o.update()
+    pass
+
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
+
+
+open_canvas()
+reset_world()
+# game loop
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
+# finalization code
+close_canvas()
---
+++ Labs/Lecture10_Character_Controller_1/control_boy.py
@@ -0,0 +1,89 @@
+from pico2d import *
+import random
+
+
+# Game object class here
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    def update(self):
+        pass
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 400, 90
+        self.frame = 0
+        self.dir = 0
+        self.action = 3
+        self.image = load_image('animation_sheet.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+
+    def handle_event(self, event):
+        pass
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, self.action * 100, 100, 100, self.x, self.y)
+
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            boy.handle_event(event)
+
+
+def reset_world():
+    global running
+    global grass
+    global team
+    global world
+    global boy
+
+    running = True
+    world = []
+
+    grass = Grass()
+    world.append(grass)
+
+    boy = Boy()
+    world.append(boy)
+
+
+
+def update_world():
+    for o in world:
+        o.update()
+    pass
+
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
+
+
+open_canvas()
+reset_world()
+# game loop
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
+# finalization code
+close_canvas()
